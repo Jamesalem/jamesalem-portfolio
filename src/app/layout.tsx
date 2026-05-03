d35d7bbca17f05bffbfc9f4e6505e3898a05e055
Jamesalem <iyanuoluwaakeju9@gmail.com>
2026-05-03 02:53:24 +0100

    Speedinsight Integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: 'Iyanuoluwa Akeju | Full-Stack Developer',
@@ -23,6 +24,7 @@ export default function RootLayout({
       <body className="bg-slate-950 text-slate-50 antialiased">
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
